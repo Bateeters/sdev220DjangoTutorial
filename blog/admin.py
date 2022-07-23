65a65c155ad2ce58701027848d073223b32c71fb
Brian <brianteetersdesign@gmail.com>
2022-07-22 23:02:01 -0400

    Django Tutorial Extensions

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # admin username: test123
 # admin password: password1
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
